d530174c86f4a81fd2950cf8b594d524182da422
Olena Babak <elena.bsh18@gmail.com>
2024-02-28 19:15:57 +0200

    new files

---
+++ L1/syntax_example.py
@@ -0,0 +1,21 @@
+import requests
+import random
+
+from main import print_hi
+
+
+# a = "hello"
+
+a = "hello"   #a is a string
+b = "hello"
+c = "hello"
+f = "hello"
+e = "hello"
+
+value_containe_one = 1
+
+def my_func():
+    print('hello')
+
+class MyClass:
+    pass
